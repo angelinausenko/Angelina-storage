fc47f74f26c0f96a5a345ca1bb76cfa553ee109b
angelinausenko <22fmif.a.usenko@std.npu.edu.ua>
2023-01-30 11:38:09 +0200

    data

---
+++ LabData2/LabData2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LabData2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Назва ноутбуку
+        string Name = "MacBook Pro";
+
+        // Процесор
+        string proccessor = "M1";
+
+        // Пам'ять (в ГБ)
+        int memory = 16;
+
+        // Операційна система
+        string system = "Ventura 13";
+
+        // Дисплей
+        string display = "13-inch";
+
+        Console.WriteLine("Назва ноутбуку " + Name);
+        Console.WriteLine("Процесор " + proccessor);
+        Console.WriteLine("Пам'ять в ГБ " + memory);
+        Console.WriteLine("Операційна система " + system);
+        Console.WriteLine("Дисплей " + display);
+    }
+}
+
