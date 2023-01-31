74ffda835da441fb2b99c5bb7eee19f225d65737
angelinausenko <22fmif.a.usenko@std.npu.edu.ua>
2023-01-31 22:01:57 +0200

    Lab5

---
+++ Laba5/Laba5/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Laba5;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //навчальна система
+
+        Console.WriteLine("Введіть дані студента \n");
+        Console.WriteLine("Прізвище: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Ім'я: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Вік: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Номер телефону: ");
+        Console.ReadLine();
+
+        //дані що стосуються навчального закладу
+
+        Console.WriteLine("Професія: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Факультет: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Курс: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Корпоративна пошта: ");
+        Console.ReadLine();
+
+
+    }
+}
+
