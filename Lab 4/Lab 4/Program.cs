1a9af8cd13cf08b839f32502c4d24aefc0a3d879
angelinausenko <22fmif.a.usenko@std.npu.edu.ua>
2023-01-31 18:12:08 +0200

    44

---
+++ Lab 4/Lab 4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab_4;
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        //число х більше або дорівнює 5,
+        //та менше або дорівнює 15
+
+        //Введення вихідних даних
+        Console.Write("x=");
+        double x = double.Parse(Console.ReadLine());
+
+        //реалізація алгоритму
+        if (x >= 5 && x <= 15)
+            Console.WriteLine("true");
+        else
+            Console.WriteLine("false");
+        //
+        Console.ReadKey();
+        
+
+
+ 
+
+
+    }
+}
+
